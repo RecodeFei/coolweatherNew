c2c135c84c806fbfd53125cb2ebfb2a06968a8f9
feikuang <feikuang@tcl.com>
2017-06-13 17:17:30 +0800

    增加工具类和数据库基本类

---
+++ app/src/main/java/app/coolweather/com/coolweathernew/db/Province.java
@@ -0,0 +1,36 @@
+package app.coolweather.com.coolweathernew.db;
+import org.litepal.crud.DataSupport;
+/**
+ * Created by feikuang on 6/13/17.
+ */
+public class Province extends DataSupport {
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    private int getId() {
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
